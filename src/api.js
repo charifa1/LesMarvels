652b4fb983ffedc02d2c11086078ddf618310aaa
Maryam Bint Ibrahim <mbintibrahim4@gmail.com>
2024-02-05 00:02:11 +0100

    Rendu Tp2

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-
+import { createHash } from 'crypto';
 
 /**
  * Récupère les données de l'endpoint en utilisant les identifiants
@@ -6,9 +6,34 @@
  * @param url l'end-point
  * @return {Promise<json>}
  */
-
 export const getData = async (url) => {
-    // A Compléter
+    const publicKey = "561ece8f131e029a49cfc0bf7db1672e";
+    const privateKey = "ed0882e95add46617750f2da836dd287d855eb22";
+    const ts = Date.now();
+    const hash = await getHash(publicKey, privateKey, ts);
+
+    const apiUrl = `${url}?apikey=${publicKey}&ts=${ts}&hash=${hash}&offset=100`;
+
+    try {
+        const response = await fetch(apiUrl);
+        const datas = await response.json();
+
+        const resultsWithThumbnail = datas.data.results.filter(character =>
+            character.thumbnail && character.thumbnail.path !== "image_not_available"
+        );
+
+        const characters = resultsWithThumbnail.map(character => ({
+            name: character.name,
+            description: character.description,
+            thumbnail: character.thumbnail,
+            imageUrl: `${character.thumbnail.path}/portrait_xlarge.${character.thumbnail.extension}`
+        }));
+
+        return characters;
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        throw error;
+    }
 }
 
 /**
@@ -20,5 +45,8 @@ export const getData = async (url) => {
  * @return {Promise<ArrayBuffer>} en hexadecimal
  */
 export const getHash = async (publicKey, privateKey, timestamp) => {
-    // A compléter
+    // exo 3
+    const hash = createHash('md5');
+    hash.update(timestamp + privateKey + publicKey);
+    return hash.digest('hex');
 }
--- src/server.js
+++ src/server.js
@@ -1,1 +1,27 @@
-console.log("Je suis Marvel")
+import fastify from 'fastify';
+import fastifyView from '@fastify/view';
+import handlebars from 'handlebars';
+import { getData } from './api.js';
+
+const server = fastify();
+
+server.register(fastifyView, {
+    engine: {
+        handlebars: handlebars
+    },
+    options: {
+        partials: {
+            header: '/templates/header.hbs',
+            footer: '/templates/footer.hbs'
+        }
+    }
+});
+
+server.get('/', async (req, res) => {
+    const characters = await getData("https://gateway.marvel.com/v1/public/characters");
+    console.log(characters);
+    return res.view('/templates/index.hbs', {characters: characters})
+
+})
+
+server.listen({ port: 3003});
